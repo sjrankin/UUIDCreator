441a85b3665cf71afe541676c4e2d006bdb82e89
Stuart Rankin <sjrankin@gmail.com>
2020-12-10 12:52:06 +0900

    Initial Commit

---
+++ UUIDCreator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UUIDCreator
+//
+//  Created by Stuart Rankin on 12/10/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
